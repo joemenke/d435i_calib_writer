69e47519c5e8dfde6e8c0e0a871639a150e0ba13
Joseph Menke <joemenke@berkeley.edu>
2019-05-08 21:30:04 -0700

    added rgb image to calibration

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@
 #include <iostream>
 #include "util/calib_writer.hpp"
 
-#define ENABLE_RGB 0
 
 int main(int argc, char *argv[]) try
 {
@@ -29,9 +28,7 @@ int main(int argc, char *argv[]) try
     cfg.enable_stream(RS2_STREAM_ACCEL, RS2_FORMAT_MOTION_XYZ32F,250);
     cfg.enable_stream(RS2_STREAM_GYRO, RS2_FORMAT_MOTION_XYZ32F,200);
 
-    if(ENABLE_RGB){
-        cfg.enable_stream(RS2_STREAM_COLOR,-1,640,480,RS2_FORMAT_BGR8,30);
-    }
+    cfg.enable_stream(RS2_STREAM_COLOR,-1,640,480,RS2_FORMAT_BGR8,30);
 
     rs2::pipeline pipe;
     //rs2::pipeline motion_pipe;
@@ -48,12 +45,15 @@ int main(int argc, char *argv[]) try
 
     auto ir2_stream = selection.get_stream(RS2_STREAM_INFRARED,2)
                              .as<rs2::video_stream_profile>();
+    auto color_stream = selection.get_stream(RS2_STREAM_COLOR)
+                             .as<rs2::video_stream_profile>();
+    // Get motion streams
     auto gyro_stream =  selection.get_stream(RS2_STREAM_GYRO)
                              .as<rs2::motion_stream_profile>();
     auto accel_stream =  selection.get_stream(RS2_STREAM_ACCEL)
                              .as<rs2::motion_stream_profile>();
 
-        //get extrinsics between cameras
+    //get extrinsics between cameras and imu
     rs2_extrinsics extrinsics = ir1_stream.get_extrinsics_to(accel_stream);
     Eigen::Vector3f T_12(extrinsics.translation[0], extrinsics.translation[1], extrinsics.translation[2]);
     Eigen::Map<Eigen::Matrix3f>R_12(extrinsics.rotation);
@@ -76,13 +76,21 @@ int main(int argc, char *argv[]) try
     tf_depth.block<3,3>(0,0)=R3_12;
     tf_depth.block<3,1>(0,3)=T3_12;
 
+    rs2_extrinsics extrinsics4 = color_stream.get_extrinsics_to(accel_stream);
+    Eigen::Vector3f T4_12(extrinsics4.translation[0], extrinsics4.translation[1], extrinsics4.translation[2]);
+    Eigen::Map<Eigen::Matrix3f>R4_12(extrinsics4.rotation);
+    Eigen::Matrix4f tf_color = Eigen::Matrix4f::Identity();
+    tf_color.block<3,3>(0,0)=R4_12;
+    tf_color.block<3,1>(0,3)=T4_12;
+
     //get camera intrinsics
     rs2_intrinsics rs_intr_ir1 = ir1_stream.get_intrinsics();
     rs2_intrinsics rs_intr_ir2 = ir2_stream.get_intrinsics();
     rs2_intrinsics rs_intr_depth = depth_stream.get_intrinsics();
+    rs2_intrinsics rs_intr_color = color_stream.get_intrinsics();
 
 
-    //compose transform mat
+    //compose calibration for slam cameras
     ark::CameraCalibration ir1_calib(tf_ir1.transpose(),rs_intr_ir1);
     ark::CameraCalibration ir2_calib(tf_ir2.transpose(),rs_intr_ir2);
 
@@ -93,9 +101,13 @@ int main(int argc, char *argv[]) try
     //Write intrinsics to file
     rec.write_camera_intrinsics_and_extrinsics(camera_calibs);
 
+    //compose calibration for additional cameras
     ark::CameraCalibration depth_calib(tf_depth.transpose(),rs_intr_depth);
+    ark::CameraCalibration color_calib(tf_color.transpose(),rs_intr_color);
+
     std::vector<ark::CameraCalibration> add_camera_calibs;
     add_camera_calibs.push_back(depth_calib); 
+    add_camera_calibs.push_back(color_calib); 
     rec.write_additional_intrinsics_and_extrinsics(add_camera_calibs);
 
     rec.write_camera_params();
@@ -139,7 +151,14 @@ int main(int argc, char *argv[]) try
     std::cout << "Distortion Model        : " << rs_intr_depth.model << std::endl;
     std::cout << "Distortion Coefficients : [" << rs_intr_depth.coeffs[0] << "," << rs_intr_depth.coeffs[1] << "," <<
         rs_intr_depth.coeffs[2] << "," << rs_intr_depth.coeffs[3] << "," << rs_intr_depth.coeffs[4] << "]" << std::endl;
+    std::cout << "----------------Color----------------\n";
+    std::cout << "Principal Point         : " << rs_intr_color.ppx << ", " << rs_intr_color.ppy << std::endl;
+    std::cout << "Focal Length            : " << rs_intr_color.fx << ", " << rs_intr_color.fy << std::endl;
+    std::cout << "Distortion Model        : " << rs_intr_color.model << std::endl;
+    std::cout << "Distortion Coefficients : [" << rs_intr_color.coeffs[0] << "," << rs_intr_color.coeffs[1] << "," <<
+        rs_intr_color.coeffs[2] << "," << rs_intr_color.coeffs[3] << "," << rs_intr_color.coeffs[4] << "]" << std::endl;
   
+
     std::cout << "---------------Extrinsics Cam1---------------\n";
     std::cout << "Translation Vector : [" << extrinsics.translation[0] << "," << extrinsics.translation[1] << "," << extrinsics.translation[2] << "]\n";
     std::cout << "Rotation Matrix    : [" << extrinsics.rotation[0] << "," << extrinsics.rotation[3] << "," << extrinsics.rotation[6] << "]\n";
@@ -156,6 +175,11 @@ int main(int argc, char *argv[]) try
     std::cout << "Rotation Matrix    : [" << extrinsics3.rotation[0] << "," << extrinsics3.rotation[3] << "," << extrinsics3.rotation[6] << "]\n";
     std::cout << "                   : [" << extrinsics3.rotation[1] << "," << extrinsics3.rotation[4] << "," << extrinsics3.rotation[7] << "]\n";
     std::cout << "                   : [" << extrinsics3.rotation[2] << "," << extrinsics3.rotation[5] << "," << extrinsics3.rotation[8] << "]" << std::endl;
+    std::cout << "---------------Extrinsics Color---------------\n";
+    std::cout << "Translation Vector : [" << extrinsics4.translation[0] << "," << extrinsics4.translation[1] << "," << extrinsics4.translation[2] << "]\n";
+    std::cout << "Rotation Matrix    : [" << extrinsics4.rotation[0] << "," << extrinsics4.rotation[3] << "," << extrinsics4.rotation[6] << "]\n";
+    std::cout << "                   : [" << extrinsics4.rotation[1] << "," << extrinsics4.rotation[4] << "," << extrinsics4.rotation[7] << "]\n";
+    std::cout << "                   : [" << extrinsics4.rotation[2] << "," << extrinsics4.rotation[5] << "," << extrinsics4.rotation[8] << "]" << std::endl;
 
 
     std::cout << "Closing Files..." << std::flush << std::endl;
